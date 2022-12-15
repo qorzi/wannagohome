3cb62551c1a5be89580a3a188392c07e8b842561
minu-j <minu.j.dev@gmail.com>
2022-12-15 18:19:05 +0900

    테마 전환버튼 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import './App.scss';
 import styled, { ThemeProvider } from 'styled-components';
-import {dark, light} from './theme/theme';
+import { dark, light } from './theme/theme';
 import { useTheme } from './theme/useTheme';
 import {
   RecoilRoot,atom,selector,useRecoilState,useRecoilValue,
 } from 'recoil';
+import { isPropertySignature } from 'typescript';
 
 function App() {
   const [themeMode, toggleTheme] = useTheme()
   const theme:any = themeMode === 'light' ? light : dark
+  console.log(theme)
 
   function Enter() {
-    
     return (
       <div>
         <h1 className='mainTitle'>
@@ -24,12 +25,10 @@ function App() {
 
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={ theme }>
         <Main>
-          <div className="App">
-            <button onClick={toggleTheme}>버튼</button>
-          </div>
           <Enter></Enter>
+          <ThemeButton onClick={ toggleTheme }></ThemeButton>
         </Main>
 
       </ThemeProvider>
@@ -44,10 +43,26 @@ const Main = styled.div`
   width: 100%;
   height: 100vh;
   text-align: center;
-  // background-color: ${props => props.theme.color.defaultBgColor};
   color: ${props => props.theme.color.defaultColor};
   background-image: 
     linear-gradient(to bottom, transparent, transparent 16.6667%, ${props => props.theme.color.defaultBgColor} 16.6667%),
     linear-gradient(to right, ${props => props.theme.color.defaultDotColor}, ${props => props.theme.color.defaultDotColor} 16.6667%, ${props => props.theme.color.defaultBgColor} 16.6667%);
   background-size: 6px 6px;
+`
+
+const ThemeButton = styled.div`
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  width: 100px;
+  height 38px;
+  background-image: url(${props => props.theme.button.themeButtonSrc});
+  background-size: cover;
+  cursor: pointer;
+
+  &:hover {
+    cursor: pointer;
+    background-image: url(${props => props.theme.button.themeButtonHoverSrc});
+    background-size: cover;
+  }
 `
